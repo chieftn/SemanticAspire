e2ff22b5b983436fd78b3d7127415c76f2a2eab2
Rick <rkessler@microsoft.com>
2025-01-24 08:15:50 -0800

    interim styles

---
+++ SemanticAspire.Web/Machines/PromptMachine.cs
@@ -0,0 +1,14 @@
+﻿namespace SemanticAspire.Web;
+public class PromptMachine(ChatApiClient chatApiClient)
+{
+    public Guid Id { get; }
+    public string Prompt { get; set; }
+
+    public string Response { get; protected set; }
+
+    public async Task PostPromptAsync()
+    {
+        var result = await chatApiClient.PostPromptAsync(this.Prompt);
+        this.Response = result.response;
+    }
+}
