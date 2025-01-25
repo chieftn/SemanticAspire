8a43b9465aa491bb4368b8d835f4fa433c2ec8bb
Rick <rkessler@microsoft.com>
2025-01-24 17:41:30 -0800

    cleanup

--- SemanticAspire.AppHost/Program.cs
+++ SemanticAspire.AppHost/Program.cs
@@ -11,12 +11,18 @@ var secrets =
 var apiService = builder.AddProject<Projects.SemanticAspire_ApiService>("apiservice")
     .WithReference(secrets);
 
-// builder.AddDockerfile("web", "../SemanticAspire.React", "DockerFile");
-
 builder.AddNpmApp("react", "../SemanticAspire.React")
     .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+// Disabling Blazor App
+//builder.AddProject<Projects.SemanticAspire_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WaitFor(cache)
+//    .WithReference(apiService)
+//    .WaitFor(apiService);
 
 builder.Build().Run();
